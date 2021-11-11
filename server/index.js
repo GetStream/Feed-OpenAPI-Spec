0fbade60c90a0d9d0f99c51e7c48745c69d45cf1
MartinCupela <martin.cupela@gmail.com>
2021-11-11 08:52:26 +0100

    separate spec definitions into separate files

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const app = express();
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const paths = require('./utils/paths')
-const swaggerDocument = YAML.load(paths.spec('openapi.yml'));
 
 const port = 3003;
+const app = express();
+const swaggerDocument = YAML.load(paths.spec('../build/openapi.yml'));
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.listen(port, function () {
     console.log('Example app listening on port ' + port);
 });
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
