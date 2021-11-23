c229038565427a286ae92072380cd4ebf54ea86f
MartinCupela <martin.cupela@gmail.com>
2021-11-23 14:55:14 +0100

    change theme, add logo, favicon

--- server/index.js
+++ server/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
+const path = require('path')
 const YAML = require('yamljs');
-const paths = require('./utils/paths')
+const paths = require('./utils/paths');
 
 const port = 3003;
 const app = express();
 const swaggerDocument = YAML.load(paths.spec('../build/openapi.yml'));
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.use('/',
+    swaggerUi.serve,
+    swaggerUi.setup(
+        swaggerDocument,
+        {
+            customSiteTitle: 'Stream Activity Feeds | OpenAPI Spec',
+            customfavIcon: 'favicon.ico',
+            customCssUrl: 'style.css',
+            customJs: 'custom.js',
+        }
+    )
+);
 
 app.listen(port, function () {
     console.log('Example app listening on port ' + port);
---
+++ server/static/custom.js
@@ -0,0 +1,10 @@
+var svg = document.querySelector('#logo')
+// document.addEventListener('DOMContentLoaded', () => {
+//     debugger
+//     var img = document.querySelector('.topbar img');
+// // var xml = (new XMLSerializer).serializeToString(svg);
+//     img.src = 'logo.svg';
+//     img.alt = 'Stream Logo'
+// // img.src = 'data:image/svg+xml;charset=utf-8,' + Buffer.from(xml, 'base64');
+// })
+
