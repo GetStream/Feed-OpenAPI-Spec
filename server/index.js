49f636798f7e7a01603b9899762aedd473f714dd
MartinCupela <32706194+MartinCupela@users.noreply.github.com>
2021-12-09 14:22:37 +0100

    Document all responses and setup the environment (#3)
    * setup express server for local development
    
    * separate spec definitions into separate files
    
    * update POST /activities
    
    * avoid keeping internal references and add --dereference option to build the spec
    
    * add /enrich/activities spec
    
    * add /og spec
    
    * add /enrich/personalization/feed endpoint spec
    
    * add /enrich/personalization/feed endpoint spec
    
    * add /feed/{feed_slug}/{user_id}/ endpoint spec
    
    * header parameters moved to paths files
    
    * add /enriched/feed/{feed_slug}/{user_id} endpoint spec
    
    * remove post /enriched/feed/{feed_slug}/{user_id} endpoint spec, rename files to match the URL path
    
    * remove post /feed/{feed_slug}/{user_id}/{activity_or_foreign_id} endpoint spec, rename file parameters/_index.yml
    
    * remove post /feed/{feed_slug}/{user_id}/followers endpoint spec
    
    * remove post /feed/{feed_slug}/{user_id}/following endpoint spec
    
    * remove post /feed/{feed_slug}/{user_id}/following/{target_feed_id} endpoint spec
    
    * remove post /feed/add_to_many endpoint spec
    
    * add /follow_many endpoint spec
    
    * move follow_many.yml and add parameters to /og endpoint spec
    
    * fix wrong ref paths
    
    * Add reaction endpoints spec
    
    * add spec for /reaction/{lookup_type}/{lookup_value}/{kind} endpoint
    
    * add spec for /collection endpoint
    
    * add spec for /collections/{collection_name}/{id} endpoint
    
    * add spec for /collections/{collection_name} endpoint
    
    * add spec for /user/{id} and /user endpoint
    
    * add spec for /iamges and/files endpoints
    
    * change theme, add logo, favicon
    
    * add specification description
    
    * sync tab colors
    
    * add comment
    
    * reorder endpoint clusters
    
    * remove unused js
    
    * add missing path descriptions
    
    * add run instructions
    
    * rename package.json script
    
    Co-authored-by: Anton Arnautov <43254280+arnautov-anton@users.noreply.github.com>
    
    * update yarn dev script
    
    Co-authored-by: Amin Mahboubi <amin@getstream.io>
    
    * use generator.getstream.io for JWT generation
    
    Co-authored-by: Amin Mahboubi <amin@getstream.io>
    
    Co-authored-by: Anton Arnautov <43254280+arnautov-anton@users.noreply.github.com>
    Co-authored-by: Amin Mahboubi <amin@getstream.io>

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const path = require('path')
+const YAML = require('yamljs');
+const paths = require('./utils/paths');
+
+const port = 3003;
+const app = express();
+const swaggerDocument = YAML.load(paths.spec('../build/openapi.yml'));
+
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.use('/',
+    swaggerUi.serve,
+    swaggerUi.setup(
+        swaggerDocument,
+        {
+            customSiteTitle: 'Stream Activity Feeds | OpenAPI Spec',
+            customfavIcon: 'favicon.ico',
+            customCssUrl: 'style.css'
+        }
+    )
+);
+
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port);
+});
